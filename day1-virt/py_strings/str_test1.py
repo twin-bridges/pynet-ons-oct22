26da01d58bdc117dd26af40bf9a215bc3c489989
Kirk Byers <ktbyers@twb-tech.com>
2022-09-22 16:59:46 -0700

    Experimenting with Git + VSCode

---
+++ day1-virt/py_strings/str_test1.py
@@ -0,0 +1,12 @@
+print ('\nHello world\n')
+x = 1
+for _ in range(100):
+    print(_)
+
+if x == 1:
+    print("Testing")
+    if x < 10:
+        print("Test again")
+elif x < 7:
+    print("Testing")
+    print("Another change")
