5a752abba2ade3c2499d3bcc0cbc462c595c0245
Kirk Byers <ktbyers@twb-tech.com>
2022-09-23 12:19:05 -0700

    More VSCode testing

--- day1-virt/py_strings/str_test1.py
+++ day1-virt/py_strings/str_test1.py
@@ -1,14 +1,19 @@
-print ('\nHello world\n')
-x = 1
+print("\nHello world\n")
+# x = 1
+x = 7
 for _ in range(100):
     print(_)
 
+# New line
 if x == 1:
     print("Testing")
     if x < 10:
         print("Test again")
+
 elif x < 7:
     print("Testing")
     print("Another change")
 
-print("Testing")
+print("Testing, but modified")
+
+print("Test again")
