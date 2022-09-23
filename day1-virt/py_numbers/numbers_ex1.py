a0a7a106359234a97a0187e9c50d5edc96f84214
Kirk Byers <ktbyers@twb-tech.com>
2022-09-23 13:25:53 -0700

    Adding Python number content

---
+++ day1-virt/py_numbers/numbers_ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+num1 = int(input("\nEnter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(f"\n\nSum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}".format(num1 * num2))
+print(f"Division: {num1 / num2:.2f}")
+print()
