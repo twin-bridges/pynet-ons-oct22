1929927b53d924f6257ac210226fce3bbd509a57
Kirk Byers <ktbyers@twb-tech.com>
2022-09-23 13:32:06 -0700

    Regex and exercise review

---
+++ day1-virt/py_regex/regex_ex1a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Use named regular expressions
+import re
+
+with open("show_int_fa4.txt") as f:
+    output = f.read()
+
+patterns = {
+    "Input": r"(?P<pkts>\d+) packets input, (?P<bytes>\d+) bytes",
+    "Output": r"(?P<pkts>\d+) packets output, (?P<bytes>\d+) bytes",
+}
+
+for label, pattern in patterns.items():
+    match = re.search(pattern, output)
+    if match:
+        print(f"\n{label}: ")
+        print(f"Packets: {match.group('pkts')}")
+        print(f"Bytes: {match.group('bytes')}\n")
