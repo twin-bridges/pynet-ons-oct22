0b23f04c477149469acaf16fc95bda63d8ca75ec
Kirk Byers <ktbyers@twb-tech.com>
2022-09-23 13:27:17 -0700

    Adding dictionary and loop content

---
+++ day1-virt/py_dict/dict_ex1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+net_device = {
+    "ip_addr": "172.30.220.1",
+    "username": "admin",
+    "password": "some_pass",
+    "vendor": "cisco",
+    "model": "3945",
+}
+
+print()
+for k, v in net_device.items():
+    print(k, v)
+
+net_device["password"] = "new_value"
+net_device["secret"] = "some_secret"
+
+device_type = net_device.get("device_type", "cisco_ios")
+print(f"\nDefault device type: {device_type}\n")
