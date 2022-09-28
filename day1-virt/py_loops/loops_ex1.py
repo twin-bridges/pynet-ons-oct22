ba2be1e4b7bf53c8f474c03c67ec095536c304f3
Kirk Byers <ktbyers@twb-tech.com>
2022-09-28 16:02:20 -0700

    Update loop exercises

--- day1-virt/py_loops/loops_ex1.py
+++ day1-virt/py_loops/loops_ex1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Range technically builds an interable and not a list
+# Range technically builds an iterable and not a list
 for i in range(1, 50):
     if i == 13:
         continue
