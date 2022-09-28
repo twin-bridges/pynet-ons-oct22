8e660321f64bc014f8a29d780003abe8cd9e4924
Kirk Byers <ktbyers@twb-tech.com>
2022-09-28 10:43:11 -0700

    Minor updates

---
+++ day1-virt/scratch/test1.py
@@ -0,0 +1,8 @@
+i = 12
+
+if i == 10:
+    print("Hello")
+elif i > 10:
+    print("Something else")
+else:
+    print("what")
--- day1-virt/str_test2.py
+++
@@ -1,7 +0,0 @@
-print("Hello world again")
-
-print("a")
-print("b")
-
-for i in range(10):
-    print(i)
