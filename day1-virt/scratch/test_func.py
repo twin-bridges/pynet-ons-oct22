2e83469f46c87dfb53c8257c040d9f0601960f49
Kirk Byers <ktbyers@twb-tech.com>
2022-09-28 18:45:14 -0700

    Add scratch code for testing functions.

---
+++ day1-virt/scratch/test_func.py
@@ -0,0 +1,6 @@
+def my_func(arg1, arg2, arg3):
+    print(f"Hello world: {arg1} {arg2} {arg3}")
+
+my_list = ["a", "bbb", "cccc"]
+my_dict = {"arg1": "zzz", "arg2": "yyy", "arg3": "xxx"}
+my_func(**my_dict)
