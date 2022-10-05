f792e88c12bcc9613263bbeb1286846d45395c56
Kirk Byers <ktbyers@twb-tech.com>
2022-10-05 12:36:32 -0700

    Adding a solution to our string exercise1

---
+++ day1-virt/py_strings/test_str_ex1.py
@@ -0,0 +1,14 @@
+
+# a. Create a python script with three strings representing three names
+# b. Print these three names out in a column 30 wide, right aligned
+# c. Execute the script verify the output
+# d. Add a prompt for a fourth name, print this out as well
+# e. Check your code into Git and GitHub
+
+name1 = "George Washington"
+name2 = "Thomas Jefferson"
+name3 = "Kirk Byers"
+
+name4 = input("Please enter a name: ")
+
+print(name4)
