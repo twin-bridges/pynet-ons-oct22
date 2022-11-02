ba5751b9231a072afa1bf326a3c92092ec886634
Kirk Byers <ktbyers@twb-tech.com>
2022-11-02 11:27:11 -0700

    Class exercises

--- day3/py_classes/classes_ex1.py
+++ day3/py_classes/classes_ex1.py
@@ -5,14 +5,15 @@ class NetDevice:
         self.username = username
         self.password = password
 
-        self.serial_number = ""
-        self.vendor = ""
-        self.model = ""
-        self.os_version = ""
-        self.uptime = ""
-
 
 my_obj1 = NetDevice(ip_addr="1.1.1.1", username="admin", password="pwd")
 my_obj2 = NetDevice(ip_addr="1.1.1.2", username="admin", password="pwd")
 my_obj3 = NetDevice(ip_addr="1.1.1.3", username="admin", password="pwd")
 my_obj4 = NetDevice(ip_addr="1.1.1.4", username="admin", password="pwd")
+
+print()
+print(f"Object: {my_obj4}")
+print(f"IP Addr: {my_obj4.ip_addr}")
+print(f"Username: {my_obj4.username}")
+print(f"Password: {my_obj4.password}")
+print()
