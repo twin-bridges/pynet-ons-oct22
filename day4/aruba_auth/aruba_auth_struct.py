f7c32892110b37bd87ed74f7a0bdb26b9f186f6a
Kirk Byers <ktbyers@twb-tech.com>
2022-11-09 15:38:29 -0800

    Structured code for aruba auth

--- day4/aruba_auth/aruba_auth_struct.py
+++ day4/aruba_auth/aruba_auth_struct.py
@@ -36,9 +36,7 @@ def auth(session, host, api_port):
         raise ValueError(f"Authentication Failed: {response.status_code}")
 
 
-def aruba_get(session, host, port, relative_url):
-
-    base_url = f"https://{host}:{port}/v1/configuration/"
+def aruba_get(session, base_url, relative_url):
     full_url = f"{base_url}{relative_url}"
     return session.get(full_url, verify=SSL_VERIFY)
 
@@ -46,43 +44,17 @@ def aruba_get(session, host, port, relative_url):
 def main():
     host = "aruba.lasthop.io"
     api_port = "4343"
+    base_url = f"https://{host}:{api_port}/v1/configuration/"
 
     session = requests.Session()
     session.headers["Accept"] = "application/json"
     auth(session, host=host, api_port=api_port)
 
     # Test a GET operation
-    # relative_url = "object/aaa_prof?config_path=/md"
-    # response = aruba_get(session, relative_url)
-    # rich.print(response.json())
+    relative_url = "object/int_vlan"
 
-    # List of all of the objects
-    # relative_url = "object"
-    # response = aruba_get(session, relative_url)
-    # rich.print(response.json())
-
-    # relative_url = "object/int_vlan"
-    # relative_url = "object/vlan_id"
-    aruba_obj = "container/WAN"
-    # aruba_obj = "object/vlan_id"
-    # aruba_obj = "object/int_vlan"
-    # aruba_obj = "object/vlan_name_id"
-    # aruba_obj = "object/int_vlan"
-    # relative_url = f"{aruba_obj}"
-    config_path = "?config_path=/mm"
-    api_type = "&type='meta-only'"
-    relative_url = f"{aruba_obj}{config_path}{api_type}"
-    response = aruba_get(session, host=host, port=api_port, relative_url=relative_url)
-    my_ds = response.json()
+    response = aruba_get(session, base_url, relative_url)
     rich.print(response.json())
-    print(relative_url)
-    # /md = managed devices
-    # config_path = "?config_path=/md"
-
-    # /mm = Common to both mobility masters
-
-    # response = aruba_get(session, relative_url)
-    # rich.print(response.json())
 
 
 if __name__ == "__main__":
