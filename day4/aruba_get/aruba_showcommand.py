d4fb059c64153e47169576b2ed07a70588cfa267
Kirk Byers <ktbyers@twb-tech.com>
2022-11-10 16:52:25 -0800

    Simple Get operations

--- day4/aruba_get/aruba_showcommand.py
+++ day4/aruba_get/aruba_showcommand.py
@@ -13,10 +13,13 @@ uid_aruba_qs = f"UIDARUBA={uid_aruba}"
 
 # Test a GET operation
 base_url = f"https://{host}:{api_port}/v1/configuration/"
-relative_url = "object/ap_group"
 
 # Adding the UID_ARUBA query string for compatibility with 8.6.X.X
-full_url = f"{base_url}{relative_url}?{uid_aruba_qs}"
+command = "show+ap+database-summary"
+command = "show+ap+radio-database"
+relative_url = f"showcommand?command={command}&{uid_aruba_qs}"
+full_url = f"{base_url}{relative_url}"
 
 response = session.get(full_url, verify=False)
 rich.print(response.json())
+
