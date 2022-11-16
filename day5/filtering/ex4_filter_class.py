6559946d1e2cdac4d71ab8ca9f4d744cba9846c0
Kirk Byers <ktbyers@twb-tech.com>
2022-11-16 08:10:26 -0800

    Filtering exercise4 class solution

--- day5/filtering/ex4_filter_class.py
+++ day5/filtering/ex4_filter_class.py
@@ -1,16 +1,20 @@
+import rich
 from getpass import getpass
 from my_aruba import ArubaAPI
-from rich import print
 
 host = "aruba.lasthop.io"
-# host = "10.5.235.12"
 username = input("Enter your username: ")
 password = getpass()
-
 aruba_conn = ArubaAPI(host=host, username=username, password=password)
 aruba_conn.auth()
+
+# Apply the Filter
+relative_url = "object/vlan_name_id"
+config_path = "/md/40Lab/VH"
+my_filter = [{"vlan_name_id.name": {"$in": ["guest"]}}]
+
 data = aruba_conn.get_request(
-    relative_url="object/hostname", config_path="/md/40Lab/VH/20:4c:03:58:70:72"
+    relative_url=relative_url, config_path=config_path, filter_obj=my_filter
 )
+rich.print(data)
 aruba_conn.logout()
-print(data)
